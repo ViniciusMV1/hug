bc41b14300bbe5e7dd0562859b054a2c1fc5ad60
Allana <allanaaquinobia@gmail.com>
2023-09-15 11:18:40 -0300

    add de afins e mais

--- src/main/java/br/com/hug/models/psicologo/Psicologo.java
+++ src/main/java/br/com/hug/models/psicologo/Psicologo.java
@@ -3,6 +3,7 @@ package br.com.hug.models.psicologo;
 import br.com.hug.models.Endereco;
 import br.com.hug.models.Pessoa;
 import jakarta.persistence.Entity;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Entity
@@ -10,12 +11,12 @@ import lombok.Data;
 public class Psicologo extends Pessoa {
     private String crp;
 
-    public Psicologo(String cpf, String nome, String telefone, Endereco endereco, String crp) {
+    public Psicologo(String cpf, String nome, String telefone, String email, Endereco endereco, String crp) {
         super(cpf, nome, email, telefone, endereco);
         this.crp = crp;
     }
 
-    public Psicologo() {
+    public Psicologo(@NotNull @NotBlank @Size(min = 12, max = 12) @Pattern(regexp = "\\d{12}") String crp, @NotNull @NotBlank @Size(max = 100) String nome, @NotNull @NotBlank @Email @Size(max = 100) String email, @NotNull Endereco endereco, @NotNull @NotBlank @Size(max = 20) String telefone) {
 
     }
 }
